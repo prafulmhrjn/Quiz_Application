0eb9f220c8e720b3c6a51d413ed6a38d2667b275
Prafulmhrjn <praful.mhrjn29@gmail.com>
2021-10-01 13:17:51 +0545

    Initial commit

---
+++ App.py
@@ -0,0 +1,10 @@
+from tkinter import *
+root = Tk()
+root.title('Quiz Application')
+root['bg']='light green'
+root.geometry("1280x680")
+
+
+label_m=Label(root,text='Quiz',font=('Arial',50),bg='dark blue')
+label_m.place(x=600,y=50)
+root.mainloop()
