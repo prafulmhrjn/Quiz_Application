53246355f94d19fe47fccafcfc0c429ba54e53c4
Prafulmhrjn <praful.mhrjn29@gmail.com>
2021-10-02 13:11:48 +0545

    initial commit

--- App.py
+++ App.py
@@ -1,16 +1,33 @@
+#import the required libraries
 from tkinter import *
+from PIL import ImageTk, Image
+
+
+
 root = Tk()
-root.title('Quiz Application')
+root.title('Guess The Sneaker')
 root['bg']='light green'
-root.geometry("1280x680")
+root.geometry("900x600")
+
+#load images
+
+img1 = Image.open("C:/Users/Prafulmhrjn/OneDrive/Desktop/dd/kicks.jpg")
+img1 = img1.resize((400,400), Image.ANTIALIAS)
+photoimg1 = ImageTk.PhotoImage(img1)
+btn = Button(root, image=photoimg1, borderwidth=0)
+btn.place(x=250, y=20)
 
 
-label_m=Label(root,text='Quiz',font=('Arial',50),bg='light green')
-label_m.place(x=600,y=50)
-label1=Label(root,text='Username',font=('Arial',30),bg='light green')
-label1.place(x=400,y=250)
-entry_1=Entry(root,font=('Arial',30))
-entry_1.place(x=500,y=250)
-button1=Button(root,text='Submit',font=('Arial',30))
-button1.place(x=600,y=350)
+#labels
+labeltext=Label(root,text='Guess The Sneaker',font=('Algerian',24),bg='light blue')
+labeltext.grid(padx=300,pady=450)
+
+#Create the buttons
+startbtn=Button(root,text='START',fg='white',font=('Algerian',20),bg='black')
+startbtn.place(x=400,y=510)
+
+
+
+
+
 root.mainloop()
