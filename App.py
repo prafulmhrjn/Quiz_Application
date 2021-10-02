b205268262804794fca7f7ac9b5e0d2afa8aa442
Prafulmhrjn <praful.mhrjn29@gmail.com>
2021-10-02 21:08:56 +0545

    initial commit

--- App.py
+++ App.py
@@ -1,6 +1,8 @@
 #import the required libraries
+import tkinter
 from tkinter import *
 from PIL import ImageTk, Image
+import random
 
 
 
@@ -9,13 +11,81 @@ root.title('Guess The Sneaker')
 root['bg']='light green'
 root.geometry("900x600")
 
+
+questions = [
+    "This is sample question 1",
+    "This is sample question 2",
+    "This is sample question 3",
+    "This is sample question 4",
+    "This is sample question 5",
+    "This is sample question 6",
+    "This is sample question 7",
+    "This is sample question 8",
+    "This is sample question 9",
+    "This is sample question 10"
+]
+
+answers_choice = [
+    ["1","2","3","4"],
+    ["1","2","3","4"],
+    ["1","2","3","4"],
+    ["1","2","3","4"],
+    ["1","2","3","4"],
+    ["1","2","3","4"],
+    ["1","2","3","4"],
+    ["1","2","3","4"],
+    ["1","2","3","4"],
+    ["1","2","3","4"]
+
+]
+
+indexes = []
+def gen():
+    global indexes
+    while(len(indexes)<5):
+        x=random.randint(0,9)
+        if x in indexes:
+            continue
+        else:
+            indexes.append(x)
+
+
+#functions
+
+def startquiz():
+    lblquestion=Label(root,text='This is a sample question',font=('Consolas',16))
+    lblquestion.pack()
+
+    radiovar = IntVar()
+    radiovar.set(-1)
+
+    r1 = tkinter.Radiobutton(root,text='sample choice 1',font=('Times',12),value=0,variable=radiovar)
+    r1.pack()
+    r2 = tkinter.Radiobutton(root, text='sample choice 2', font=('Times', 12), value=1, variable=radiovar)
+    r2.pack()
+    r3 = tkinter.Radiobutton(root, text='sample choice 3', font=('Times', 12), value=2, variable=radiovar)
+    r3.pack()
+    r4 = tkinter.Radiobutton(root, text='sample choice 4', font=('Times', 12), value=3, variable=radiovar)
+    r4.pack()
+
+
+
+
+def pressedstart():
+    btnimg.destroy()
+    labeltext.destroy()
+    startbtn.destroy()
+    startquiz()
+
+
+
 #load images
 
 img1 = Image.open("C:/Users/Prafulmhrjn/OneDrive/Desktop/dd/kicks.jpg")
 img1 = img1.resize((400,400), Image.ANTIALIAS)
 photoimg1 = ImageTk.PhotoImage(img1)
-btn = Button(root, image=photoimg1, borderwidth=0)
-btn.place(x=250, y=20)
+btnimg = Button(root, image=photoimg1, borderwidth=0)
+btnimg.place(x=250, y=20)
 
 
 #labels
@@ -23,7 +93,7 @@ labeltext=Label(root,text='Guess The Sneaker',font=('Algerian',24),bg='light blu
 labeltext.grid(padx=300,pady=450)
 
 #Create the buttons
-startbtn=Button(root,text='START',fg='white',font=('Algerian',20),bg='black')
+startbtn=Button(root,text='START',fg='white',font=('Algerian',20),bg='black',command=pressedstart)
 startbtn.place(x=400,y=510)
 
 
